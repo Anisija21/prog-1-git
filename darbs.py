d03304488367967798bd08239239c3560201e3a4
Anisija21 <77109056+Anisija21@users.noreply.github.com>
2021-02-18 09:05:50 +0200

    Pievienots darbs.py

---
+++ darbs.py
@@ -0,0 +1,12 @@
+a = 10
+b = 30
+c = 20
+
+if b > a:
+  print("b ir lielaks par a")
+else:
+  print("b ir lielaks par a")
+if c < b:
+  print("c ir mazaks par b")
+else: 
+  print("c ir mazaks par b")
